7cecefa1516bdf8f6e61716ffcf5b96bb81fb515
Pedro <pedrorodl07@gmail.com>
2025-03-27 15:22:01 -0300

    criando o arquivo com oq tem no site

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  class TasksController < ApplicationController
+    before_action :set_task, only: [ :update, :destroy ]
+
+    def index
+      @tasks = Task.all
+    end
+
+    def create
+      @task = Task.create(task_params)
+    end
+
+    def update
+      @task.update!(task_params)
+    end
+
+    def destroy
+      @task.destroy!
+    end
+
+    private
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:description, :completed)
+    end
+  end
 end
