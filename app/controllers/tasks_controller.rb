00250b3dae495ef3bfaccaafba0e4114e8dcbaf1
Pedro <pedrorodl07@gmail.com>
2025-03-27 15:59:57 -0300

    concertando erros

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,28 @@
 class TasksController < ApplicationController
-  class TasksController < ApplicationController
-    before_action :set_task, only: [ :update, :destroy ]
+  before_action :set_task, only: [ :update, :destroy ]
 
-    def index
-      @tasks = Task.all
-    end
+  def index
+    @tasks = Task.all
+  end
 
-    def create
-      @task = Task.create(task_params)
-    end
+  def create
+    @task = Task.create!(task_params)
+  end
+  def update
+    @task.update!(task_params)
+  end
 
-    def update
-      @task.update!(task_params)
-    end
+  def destroy
+    @task.destroy!
+  end
 
-    def destroy
-      @task.destroy!
-    end
+  private
 
-    private
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    def set_task
-      @task = Task.find(params[:id])
-    end
-
-    def task_params
-      params.require(:task).permit(:description, :completed)
-    end
+  def task_params
+    params.require(:task).permit(:description, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :tasks
   root "tasks#index"
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 end
